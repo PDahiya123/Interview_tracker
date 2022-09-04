d574f64c11ed2df4dc4cdc7f119a0eca4f7346ac
PDahiya123 <pranshudahiyaiitg@gmail.com>
2022-09-04 16:39:27 +0530

    added files

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoURI : 'mongodb+srv://pranshu:JAIBB@cluster0.am7f4.mongodb.net/interview_tracker?retryWrites=true&w=majority',
+    ADMIN_EMAIL: 'pd@admin.com',
+    ADMIN_PASSWORD: 'testjs' || 'passjs'
+
+
+}
